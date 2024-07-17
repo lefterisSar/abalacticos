4bd850025f929a2b0c67e64a1e61fa0082c949a3
Lefteris Saridakis <esaridakis@tomsawyer.com>
2024-07-17 21:12:45 +0300

    - Add positions in each team.

--- abalacticos_front/src/components/TeamSelection.jsx
+++ abalacticos_front/src/components/TeamSelection.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Box, Grid, List, ListItem, ListItemText } from '@mui/material';
+import { Grid, List, ListItem, ListItemText } from '@mui/material';
+
+const positions = ['GK', 'DR', 'DC', 'DL', 'MR', 'MC', 'ML', 'FC'];
 
 const TeamSelection = ({ teamA, teamB }) => {
+    const renderPlayerWithPosition = (player, position) => (
+        <ListItem key={player.id}>
+            <ListItemText primary={`${player.name} ${player.surname}`} secondary={position} />
+        </ListItem>
+    );
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={6}>
                 <h3>Team A</h3>
                 <List>
-                    {teamA.map(player => (
-                        <ListItem key={player.id}>
-                            <ListItemText primary={`${player.name} ${player.surname}`} />
-                        </ListItem>
-                    ))}
+                    {teamA.map((player, index) => renderPlayerWithPosition(player, positions[index]))}
                 </List>
             </Grid>
             <Grid item xs={6}>
                 <h3>Team B</h3>
                 <List>
-                    {teamB.map(player => (
-                        <ListItem key={player.id}>
-                            <ListItemText primary={`${player.name} ${player.surname}`} />
-                        </ListItem>
-                    ))}
+                    {teamB.map((player, index) => renderPlayerWithPosition(player, positions[index]))}
                 </List>
             </Grid>
         </Grid>
