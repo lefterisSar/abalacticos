db0570ccd4b70522fcd96ff386bea6bf84c979f4
Lefteris Saridakis <saridakiselef@hotmail.com>
2024-10-04 17:20:34 +0300

    - Make actual SoccerField in the page.

--- abalacticos_front/src/components/AdminDashboardComponents/SoccerField.jsx
+++ abalacticos_front/src/components/AdminDashboardComponents/SoccerField.jsx
@@ -2,6 +2,8 @@
 import React, {useEffect, useState} from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
 import axios from "axios"; // Import the useLocation hook
+import './soccerfield.css';
+
 
 const SoccerField = () => {
     const location = useLocation(); // Access the location object
@@ -67,8 +69,44 @@ const SoccerField = () => {
                     <div>No players found</div>
                 )}
             </div>
-        </div>
-    );
+            <section className="pitch">
+                <div className="field left">
+                    <div className="penalty-area">
+                    </div>
+                </div>
+                <div className="field right">
+                    <div className="penalty-area">
+                    </div>
+                </div>
+                <div className="center-circle"></div>
+                <div className="home-team">
+                    <div className="player one"></div>
+                    <div className="player two"></div>
+                    <div className="player three"></div>
+                    {/*<div className="player four"></div>*/}
+                    <div className="player five"></div>
+                    <div className="player six"></div>
+                    <div className="player seven"></div>
+                    <div className="player eight"></div>
+                    {/*<div className="player nine"></div>*/}
+                    {/*<div className="player ten"></div>*/}
+                    <div className="player eleven"></div>
+                </div>
+                <div className="visitor-team">
+                    <div className="player one"></div>
+                    <div className="player two"></div>
+                    <div className="player three"></div>
+                    {/*<div className="player four"></div>*/}
+                    <div className="player five"></div>
+                    <div className="player six"></div>
+                    <div className="player seven"></div>
+                    <div className="player eight"></div>
+                    {/*<div className="player nine"></div>*/}
+                    {/*<div className="player ten"></div>*/}
+                    <div className="player eleven"></div>
+                </div>
+            </section>
+        </div>);
 };
 
 export default SoccerField;
