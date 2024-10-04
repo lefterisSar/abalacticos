5103a57504e97eabe8bd1ae286c874d0ca78e593
Lefteris Saridakis <saridakiselef@hotmail.com>
2024-10-04 21:01:30 +0300

    - Use Lombok to simplify Setters and Getters and constructors and get less verbose code. - Refactor code a bit.

--- src/main/java/com/example/abalacticos/model/Dtos/BanRequestDto.java
+++ src/main/java/com/example/abalacticos/model/Dtos/BanRequestDto.java
@@ -1,35 +1,15 @@
 package com.example.abalacticos.model.Dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 public class BanRequestDto {
     private LocalDateTime banStartDate;
     private LocalDateTime banEndDate;
     private String banReason;
-
-    // Getters and setters
-    public LocalDateTime getBanStartDate() {
-        return banStartDate;
-    }
-
-    public void setBanStartDate(LocalDateTime banStartDate) {
-        this.banStartDate = banStartDate;
-    }
-
-    public LocalDateTime getBanEndDate() {
-        return banEndDate;
-    }
-
-    public void setBanEndDate(LocalDateTime banEndDate) {
-        this.banEndDate = banEndDate;
-    }
-
-    public String getBanReason() {
-        return banReason;
-    }
-
-    public void setBanReason(String banReason) {
-        this.banReason = banReason;
-    }
 }
 
